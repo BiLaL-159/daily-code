5999ca7cd2427742e524ace3e182040bd927fcaa
BILAL <bilalaliofficial1@gmail.com>
2024-08-31 12:30:08 +0530

    Added Memoized LCS

--- DP/LCS.java
+++ DP/LCS.java
@@ -17,7 +17,32 @@ public class LCS {
         }
     }
 
+        int[][] dp;
+        public int longestCommonSubsequenceMemoized(String text1, String text2){
+            dp= new int[text1.length()+1][text2.length()+1];
+            for(int i=0; i<text1.length()+1; i++){
+                for(int j=0; j<text2.length()+1;j++){
+                    dp[i][j]=-1;
+                }
+            }
+            return solveLCSMemo(text1,text2, text1.length(),text2.length());
 
+        }
+        public int solveLCSMemo(String text1, String text2, int n, int m){
+            if(n==0 || m==0){
+                return 0;
+            }
+
+            if(dp[n][m]!=-1){
+                return dp[n][m];
+            }
+            if(text1.charAt(n-1)==text2.charAt(m-1)){
+                return dp[n][m]= 1+ solveLCSMemo(text1,text2,n-1,m-1);
+            }
+            else{
+                return dp[n][m]= Math.max(solveLCSMemo(text1,text2,n-1,m), solveLCSMemo(text1,text2,n,m-1));
+            }
+        }
 
 
 }
