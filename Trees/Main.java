46eb0ec42eb0dc6e744b146e6605dcd73eb80b4a
BILAL <bilalaliofficial1@gmail.com>
2024-09-05 11:30:23 +0530

    Added Trees

--- Trees/Main.java
+++ Trees/Main.java
@@ -1,5 +1,11 @@
 package Trees;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        Tree mytree= new Tree();
+        mytree.populate(in);
+    }
 }
